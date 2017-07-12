0a0d969109bc837b61b55b57a10b5a58b74b57c1
Bruno Vercelino da Hora <brunodahora@trackmob.com.br>
2017-07-12 07:33:26 -0300

    Using custom drawer and refactoring file for Lint

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,36 +1,59 @@
 import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
 import { MaterialIcons } from '@expo/vector-icons';
-import HomeNavigator from './HomeNavigator'
-import StudiesNavigator from './StudiesNavigator'
-import GroupsNavigator from './GroupsNavigator'
+import Drawer from '../components/Drawer';
+import HomeNavigator from './HomeNavigator';
+import StudiesNavigator from './StudiesNavigator';
+import GroupsNavigator from './GroupsNavigator';
+import AboutNavigator from './AboutNavigator';
+import { colors } from '../helpers/Constants';
 
-export default MainDrawerNavigator = DrawerNavigator({
-  Home: {
-    screen: HomeNavigator,
-    navigationOptions: {
-      drawerLabel: 'Home',
-      drawerIcon: ({ tintColor }) => (
-        <MaterialIcons name="home" size={24} color={tintColor} />
-      ),
+const MainDrawerNavigator = DrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerLabel: 'Home',
+        drawerIcon: ({ tintColor }) => (
+          <MaterialIcons name="home" size={24} color={tintColor} />
+        ),
+      },
+    },
+    StudiesList: {
+      screen: StudiesNavigator,
+      navigationOptions: {
+        drawerLabel: 'Estudos',
+        drawerIcon: ({ tintColor }) => (
+          <MaterialIcons name="insert-drive-file" size={24} color={tintColor} />
+        ),
+      },
+    },
+    GroupsList: {
+      screen: GroupsNavigator,
+      navigationOptions: {
+        drawerLabel: 'Grupos',
+        drawerIcon: ({ tintColor }) => (
+          <MaterialIcons name="group" size={24} color={tintColor} />
+        ),
+      },
+    },
+    About: {
+      screen: AboutNavigator,
+      navigationOptions: {
+        drawerLabel: 'Sobre a Rede',
+        drawerIcon: ({ tintColor }) => (
+          <MaterialIcons name="info" size={24} color={tintColor} />
+        ),
+      },
     },
   },
-  StudiesList: {
-    screen: StudiesNavigator,
-    navigationOptions: {
-      drawerLabel: 'Estudos',
-      drawerIcon: ({ tintColor }) => (
-        <MaterialIcons name="insert-drive-file" size={24} color={tintColor} />
-      ),
+  {
+    contentComponent: props => <Drawer {...props} />,
+    contentOptions: {
+      activeTintColor: colors.gray,
+      inactiveTintColor: colors.white,
     },
   },
-  GroupsList: {
-    screen: GroupsNavigator,
-    navigationOptions: {
-      drawerLabel: 'Grupos',
-      drawerIcon: ({ tintColor }) => (
-        <MaterialIcons name="group" size={24} color={tintColor} />
-      ),
-    },
-  }
-});
+);
+
+export default MainDrawerNavigator;
