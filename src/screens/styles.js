3267cb333e9bd394804759ebf70038d3621a7131
Bruno Vercelino da Hora <brunodahora@trackmob.com.br>
2017-07-12 07:32:17 -0300

    Added content to Home page with basic information

--- src/screens/styles.js
+++ src/screens/styles.js
@@ -1,12 +1,38 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../helpers/Constants'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.gray,
+    margin: 10,
+  },
+  bodyText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: colors.gray,
+    marginBottom: 10,
+  },
+  roundedIcon: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: colors.main,
+    borderRadius: 50
+  }
 });
 
 export default styles;
