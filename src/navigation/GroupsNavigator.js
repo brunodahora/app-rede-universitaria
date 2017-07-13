490984219e1d38d67ff7650fef633d7352217fc7
brunodahora <brunodahora@trackmob.com.br>
2017-07-13 15:08:55 -0300

    Added Detail to Navigators

--- src/navigation/GroupsNavigator.js
+++ src/navigation/GroupsNavigator.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import Touchable from 'react-native-platform-touchable'
+import Touchable from 'react-native-platform-touchable';
 import { MaterialIcons } from '@expo/vector-icons';
-import GroupsList from '../screens/GroupsList'
-import { colors } from '../helpers/Constants'
+import GroupsList from '../screens/GroupsList';
+import GroupDetail from '../screens/GroupDetail';
+import { colors } from '../helpers/Constants';
 
-export default GroupsNavigator = StackNavigator({
-  Home: {
+const GroupsNavigator = StackNavigator({
+  GroupsList: {
     screen: GroupsList,
     navigationOptions: ({ navigation }) => ({
       title: 'Grupos',
-      headerLeft: (<Touchable onPress={() => navigation.navigate('DrawerOpen')}>
-                  <MaterialIcons
-                    name="menu"
-                    size={24}
-                    color={colors.white}
-                    style={{margin: 10}}
-                  />
-                </Touchable>),
+      headerLeft: (
+        <Touchable onPress={() => navigation.navigate('DrawerOpen')}>
+          <MaterialIcons
+            name="menu"
+            size={24}
+            color={colors.white}
+            style={{ margin: 10 }}
+          />
+        </Touchable>
+      ),
       headerTintColor: colors.white,
       headerStyle: {
-        backgroundColor: colors.main
-      }
-    })
+        backgroundColor: colors.main,
+      },
+    }),
+  },
+  GroupDetail: {
+    screen: GroupDetail,
+    navigationOptions: () => ({
+      title: 'Grupo',
+      headerTintColor: colors.white,
+      headerStyle: {
+        backgroundColor: colors.main,
+      },
+    }),
   },
 });
+export default GroupsNavigator;
--- src/navigation/StudiesNavigator.js
+++ src/navigation/StudiesNavigator.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import Touchable from 'react-native-platform-touchable'
+import Touchable from 'react-native-platform-touchable';
 import { MaterialIcons } from '@expo/vector-icons';
-import StudiesList from '../screens/StudiesList'
-import { colors } from '../helpers/Constants'
+import StudiesList from '../screens/StudiesList';
+import StudyDetail from '../screens/StudyDetail';
+import { colors } from '../helpers/Constants';
 
-export default StudiesNavigator = StackNavigator({
+const StudiesNavigator = StackNavigator({
   StudiesList: {
     screen: StudiesList,
     navigationOptions: ({ navigation }) => ({
       title: 'Estudos',
-      headerLeft: (<Touchable onPress={() => navigation.navigate('DrawerOpen')}>
-                  <MaterialIcons
-                    name="menu"
-                    size={24}
-                    color={colors.white}
-                    style={{margin: 10}}
-                  />
-                </Touchable>),
+      headerLeft: (
+        <Touchable onPress={() => navigation.navigate('DrawerOpen')}>
+          <MaterialIcons
+            name="menu"
+            size={24}
+            color={colors.white}
+            style={{ margin: 10 }}
+          />
+        </Touchable>
+      ),
       headerTintColor: colors.white,
       headerStyle: {
-        backgroundColor: colors.main
-      }
-    })
+        backgroundColor: colors.main,
+      },
+    }),
+  },
+  StudyDetail: {
+    screen: StudyDetail,
+    navigationOptions: () => ({
+      title: 'Estudo',
+      headerTintColor: colors.white,
+      headerStyle: {
+        backgroundColor: colors.main,
+      },
+    }),
   },
 });
+export default StudiesNavigator;
