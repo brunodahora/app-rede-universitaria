beabbd619131f5c5eb6a40d427c069471c5914c0
brunodahora <brunodahora@trackmob.com.br>
2017-07-13 15:06:32 -0300

    Added apisauce to Reactotron

--- src/helpers/ReactotronConfig.js
+++ src/helpers/ReactotronConfig.js
@@ -1,6 +1,7 @@
 import Reactotron, { trackGlobalErrors } from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
+import apisaucePlugin from 'reactotron-apisauce';
 
 const reactotron = Reactotron.configure({ name: 'Rede Universitária' })
   .useReactNative()
