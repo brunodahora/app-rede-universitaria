b6b1fbafb94c6fbd1544820a487eb67d1863eccd
Bruno Vercelino da Hora <brunodahora@trackmob.com.br>
2017-07-13 23:26:13 -0300

    Fixed eslint format and Header name

--- src/navigation/AboutNavigator.js
+++ src/navigation/AboutNavigator.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import Touchable from 'react-native-platform-touchable'
+import Touchable from 'react-native-platform-touchable';
 import { MaterialIcons } from '@expo/vector-icons';
-import About from '../screens/About'
-import { colors } from '../helpers/Constants'
+import About from '../screens/About';
+import { colors } from '../helpers/Constants';
 
-export default AboutNavigator = StackNavigator({
+const AboutNavigator = StackNavigator({
   Home: {
     screen: About,
     navigationOptions: ({ navigation }) => ({
-      title: 'Home',
-      headerLeft: (<Touchable onPress={() => navigation.navigate('DrawerOpen')}>
-                  <MaterialIcons
-                    name="menu"
-                    size={24}
-                    color={colors.white}
-                    style={{margin: 10}}
-                  />
-                </Touchable>),
+      title: 'Sobre',
+      headerLeft: (
+        <Touchable onPress={() => navigation.navigate('DrawerOpen')}>
+          <MaterialIcons
+            name="menu"
+            size={24}
+            color={colors.white}
+            style={{ margin: 10 }}
+          />
+        </Touchable>
+      ),
       headerTintColor: colors.white,
       headerStyle: {
-        backgroundColor: colors.main
-      }
-    })
+        backgroundColor: colors.main,
+      },
+    }),
   },
 });
+export default AboutNavigator;
