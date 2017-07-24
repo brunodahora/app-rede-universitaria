346e80917b728cf26c6181264cec937f476b4ad6
Bruno Vercelino da Hora <brunodahora@trackmob.com.br>
2017-07-24 07:58:24 -0300

    Configuring persistStore (not working yet) and adding sentry

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import Sentry from 'sentry-expo';
 import createStore from './src/store/createStore';
 import rootSaga from './src/store/sagas';
 import Reactotron from './src/helpers/ReactotronConfig';
 
 import Main from './src/Main';
 
-export const store = createStore();
+const store = createStore();
+
+store.runSaga(rootSaga);
+persistStore(store);
 
 export const getStore = () => store;
 
-store.runSaga(rootSaga);
+Sentry.config('https://ba14379aab3d432cb98d7db6061d916f:3a079af9c532498d9ef9464f004a11ed@sentry.io/194916').install();
 
 export default class App extends React.Component {
   componentDidMount() {
-    Reactotron.connect();
+    /* eslint-disable no-undef */
+    if (__DEV__) {
+      Reactotron.connect();
+    }
   }
 
   render() {
