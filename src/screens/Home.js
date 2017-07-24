82e41308da9e5c3cda69744d71d0174b0d8f877b
brunodahora <brunodahora@trackmob.com.br>
2017-07-24 13:00:53 -0300

    Adding view around Home icons to fix roundedCorners in iOS

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -37,30 +37,33 @@ export default class Home extends React.Component {
             resizeMode={Expo.Video.RESIZE_MODE_CONTAIN}
           /> */}
           <View style={styles.body}>
-            <MaterialIcons
-              name="lightbulb-outline"
-              size={50}
-              color={colors.white}
-              style={styles.roundedIcon}
-            />
+            <View style={styles.roundedIcon}>
+              <MaterialIcons
+                name="lightbulb-outline"
+                size={50}
+                color={colors.white}
+              />
+            </View>
             <Text style={[styles.bodyText, styles.centerText]}>
               A ideia da rede é promover, criar e ajudar grupos de estudantes que se reúnem nas universidades. Conheça!
             </Text>
-            <MaterialIcons
-              name="chat-bubble-outline"
-              size={50}
-              color={colors.white}
-              style={styles.roundedIcon}
-            />
+            <View style={styles.roundedIcon}>
+              <MaterialIcons
+                name="chat-bubble-outline"
+                size={50}
+                color={colors.white}
+              />
+            </View>
             <Text style={[styles.bodyText, styles.centerText]}>
               Visite nosso blog para ficar por dentro das novidades, ler testemunhos  e outras histórias da rede universitária.
             </Text>
-            <MaterialIcons
-              name="school"
-              size={50}
-              color={colors.white}
-              style={styles.roundedIcon}
-            />
+            <View style={styles.roundedIcon}>
+              <MaterialIcons
+                name="school"
+                size={50}
+                color={colors.white}
+              />
+            </View>
             <Text style={[styles.bodyText, styles.centerText]}>
               Conheça as igrejas que já estão participando da Rede, a sua Igreja também pode participar! Clique e saiba como.
             </Text>
